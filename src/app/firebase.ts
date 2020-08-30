39d084ac37d52855dd56f4422605a2df85ba68c0
Ernesto Resende <ernestoresende@hotmail.com>
2020-08-30 18:38:26 -0300

    initial firebase configuration

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyCw7PA0yayHbPUvSfqjxOm5KRw4mNKS380",
+  authDomain: "forum-testing-54efb.firebaseapp.com",
+  databaseURL: "https://forum-testing-54efb.firebaseio.com",
+  projectId: "forum-testing-54efb",
+  storageBucket: "forum-testing-54efb.appspot.com",
+  messagingSenderId: "1008606151488",
+  appId: "1:1008606151488:web:d4a175525412836443f0a5",
+  measurementId: "G-35YG01HXYY"
+}
+
+export default config 
